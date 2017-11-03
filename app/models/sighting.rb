4aa9b3f427e5cd6ff8a2a480fbb46c7a159c69aa
chrisjamesr <chrisjames.r@gmail.com>
2017-11-03 12:47:57 -0400

    add validation to object model

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,6 @@
 class Sighting < ActiveRecord::Base
+validates_presence_of :user_id, :bird_id
+# Modify to validate bird_id or bird url to find unknown bird from photo
 belongs_to :bird
 belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, :password
+  validates_uniqueness_of :username
   has_secure_password
   has_many :sightings
   has_many :birds, through: :sightings
