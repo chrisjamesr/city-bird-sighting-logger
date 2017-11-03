83629a655a7f4361be6388d2748cc3431cff2ee9
chrisjamesr <chrisjames.r@gmail.com>
2017-11-03 13:08:45 -0400

    Add bird index, new and show routes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,14 +1,26 @@
 class BirdsController < ApplicationController
 
+  
+  get '/birds' do
+    @birds = Bird.all
+    erb :'/birds/index'
+  end
+
   get '/birds/new' do
     erb :'/birds/new'
   end
 
-  get '/birds/' do
-    erb :'/birds/index'
+  post '/birds/new' do
+    bird = Bird.new(params[:bird])
+    if bird.save
+      redirect "/birds/#{bird.id}"
+    else
+      redirect '/birds/new'  
+    end
   end
 
   get '/birds/:id' do
+    @bird = Bird.find(params[:id])
     erb :'/birds/show'
   end
 
