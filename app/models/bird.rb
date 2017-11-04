7adc840d17aa62635ec1ca6f702f8c4fc3135abd
chrisjamesr <chrisjames.r@gmail.com>
2017-11-04 19:00:58 -0400

    add slug method to bird model

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -3,5 +3,11 @@ has_many :sightings
 has_many :users, through: :birds
 
 
+ def slug 
+    username.downcase.gsub(" ","+")
+  end
 
+  def self.find_by_slug(slug)
+    User.all.find{|username| username.slug == slug}
+  end
 end
