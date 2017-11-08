a2d4f7d797b124b07495e9ddb32cf2c399d64279
chrisjamesr <chrisjames.r@gmail.com>
2017-11-07 20:53:51 -0500

    add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,18 +13,16 @@ class UsersController < ApplicationController
     user = User.new(params[:user])
     if user.save
       session[:user_id] = user.id
-      # flash[:message] = "Thanks for signing up!"
-      redirect "/users/#{user.id}", flash[:notice] = "Success"
+      redirect "/users/#{user.id}", flash[:success] = "Welcome #{user.username}"
     else
       redirect '/signup', flash[:error] = user.errors.full_messages.to_sentence
     end
   end  
 
   get '/login' do
-    binding.pry
     if logged_in?
       user = User.find(session[:user_id])
-      redirect "/users/#{user.id}", flash[:notice] = "Already signed in as #{user.username}"
+      redirect "/users/#{user.id}", flash[:error] = "Already signed in as #{user.username}"
     else  
       erb :'/users/login' 
     end
@@ -33,11 +31,10 @@ class UsersController < ApplicationController
    post '/login' do
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      
-      redirect "/users/#{user.id}"
+      session[:user_id] = user.id      
+      redirect "/users/#{user.id}", flash[:success] = "Welcome #{user.username}"
     else
-      redirect '/login'
+      redirect '/login', flash[:error] = user.errors.full_messages.to_sentence
     end
   end 
 
@@ -61,7 +58,7 @@ class UsersController < ApplicationController
   # unnecessary
   get '/users/:id/edit' do
     @user = User.find(params[:id])
-    redirect "/users/#{@user.id}"
+    redirect "/users/#{@user.id}", flash[:error] = "That function is not currently available"
     # erb :'/users/edit'
   end  
 
@@ -79,9 +76,9 @@ class UsersController < ApplicationController
 
 
   get '/logout' do
-      if logged_in?
+    if logged_in?
       session.clear
-      redirect '/login', 
+      redirect '/login'
     else
       redirect '/'
     end
