561f248c56f399705e55173d2b5c615843d004b9
chrisjamesr <chrisjames.r@gmail.com>
2017-11-08 18:33:17 -0500

    add method to grab wikipedia summary and image

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -3,35 +3,34 @@ require 'wikipedia'
 class Bird < ActiveRecord::Base
 validates_presence_of :species, allow_blank: false
 validates_uniqueness_of :species
-
+after_initialize :set_wiki_info
 has_many :sightings
 has_many :users, through: :birds
 
+  def set_wiki_info
+    self.update(summary: summary, image_url: main_image_url)
+  end  
 
-
- def slug 
-    username.downcase.gsub(" ","-")
+  def wiki_scrape
+    Wikipedia.find( self.species ) 
+  end
+    
+  def main_image_url
+    wiki_scrape.main_image_url
   end
 
-  def self.find_by_slug(slug)
-    User.all.find{|username| username.slug == slug}
+  def summary
+    wiki_scrape.summary
   end
 
-  def full_page_info
-      page = Wikipedia.find( self.species )
-    end  
 
-    def main_image_url
-      full_page_info.main_image_url
-    end
+ # def slug 
+ #    username.downcase.gsub(" ","-")
+ #  end
 
-    def all_images
-      full_page_info.images
-    end   
-
-    def summary
-      full_page_info.summary
-    end
+ #  def self.find_by_slug(slug)
+ #      User.all.find{|username| username.slug == slug}
+ #  end
 
 
 
