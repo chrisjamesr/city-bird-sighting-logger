e264edcc265c9309ffab6fed38be669d7ef2c4bf
chrisjamesr <chrisjames.r@gmail.com>
2017-11-08 13:30:43 -0500

    add methods for wikipedia client gem to access bird info from wikipedia

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,9 +1,12 @@
+require 'wikipedia'
+
 class Bird < ActiveRecord::Base
 validates_presence_of :species, allow_blank: false
 validates_uniqueness_of :species
 
 has_many :sightings
 has_many :users, through: :birds
+
 
 
  def slug 
@@ -14,5 +17,23 @@ has_many :users, through: :birds
     User.all.find{|username| username.slug == slug}
   end
 
+  def full_page_info
+      page = Wikipedia.find( self.species )
+    end  
 
-end
+    def main_image_url
+      full_page_info.main_image_url
+    end
+
+    def all_images
+      full_page_info.images
+    end   
+
+    def summary
+      full_page_info.summary
+    end
+
+
+
+end # End of Class
+
