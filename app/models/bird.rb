dac9522f582a9597af8549d7caea762a8bd7d00a
chrisjamesr <chrisjames.r@gmail.com>
2017-11-08 21:29:13 -0500

    remove helper methods to new module, include new Wiki module

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,38 +1,14 @@
 require 'wikipedia'
 
 class Bird < ActiveRecord::Base
-validates_presence_of :species, allow_blank: false
-validates_uniqueness_of :species
-after_initialize :set_wiki_info
-has_many :sightings
-has_many :users, through: :birds
+  extend Wiki::ClassMethods
+  include Wiki::InstanceMethods
 
-  def set_wiki_info
-    self.update(summary: summary, image_url: main_image_url)
-  end  
-
-  def wiki_scrape
-    Wikipedia.find( self.species ) 
-  end
-    
-  def main_image_url
-    wiki_scrape.main_image_url
-  end
-
-  def summary
-    wiki_scrape.summary
-  end
-
-
- # def slug 
- #    username.downcase.gsub(" ","-")
- #  end
-
- #  def self.find_by_slug(slug)
- #      User.all.find{|username| username.slug == slug}
- #  end
-
-
+  validates_presence_of :species, allow_blank: false
+  validates_uniqueness_of :species
+  after_initialize :set_wiki_info
+  has_many :sightings
+  has_many :users, through: :birds
 
 end # End of Class
 
