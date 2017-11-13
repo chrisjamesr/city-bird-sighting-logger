558e75965e3a9ce8c4ec8c97978b1c06b4a2e17f
chrisjamesr <chrisjames.r@gmail.com>
2017-11-13 09:52:05 -0500

    add column to table for user creation date

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ActiveRecord::Base
   validates_presence_of :username, :password, allow_blank: false
   validates :username, uniqueness: { case_sensitive: false }
+  after_initialize :signed_up
   has_secure_password
   has_many :sightings
   has_many :birds, through: :sightings
 
   # add avatar to database
-  
+  def signed_up
+   self.update(member_since: Time.current)
+  end
+
+  def display_date
+    self.member_since.strftime("%B %Y")
+  end
+
   def slug 
     username.downcase.gsub(" ","-")
   end
