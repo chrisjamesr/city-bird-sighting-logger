5600ead7f802863be92e1f4a854e7a70f16173af
chrisjamesr <chrisjames.r@gmail.com>
2017-11-13 09:53:29 -0500

    create seeds file for users

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+users = User.create([
+  {username: "Dan", password: "serena"},
+  {username: "Nate", password: "jenny"},
+  {username: "Blair", password: "chuck"},
+  {username: "Jenny", password: "nate"},
+  {username: "Vanessa", password: "dan"},
+  {username: "Chuck", password: "money"}
+  ])
+
