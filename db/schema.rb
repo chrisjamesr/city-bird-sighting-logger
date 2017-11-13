1d4f87f537bc13d03e12124e8df406395d403b9b
chrisjamesr <chrisjames.r@gmail.com>
2017-11-13 08:23:14 -0500

    create database migration for datetime column users, modify secondary header styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108223008) do
+ActiveRecord::Schema.define(version: 20171113131723) do
 
   create_table "birds", force: :cascade do |t|
     t.string "species"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20171108223008) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.datetime "member_since"
   end
 
 end
