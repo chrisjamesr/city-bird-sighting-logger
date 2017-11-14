091165d55d1ba209398af5cfd9827e5cffd4cdfe
chrisjamesr <chrisjames.r@gmail.com>
2017-11-13 19:18:45 -0500

    add method to update record with formatted date time

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,7 +1,16 @@
 class Sighting < ActiveRecord::Base
 validates_presence_of :user_id, :bird_id, :location, :time_date, allow_blank: false
-# Modify to validate bird_id or bird url to find unknown bird from photo
+after_initialize :display_time
+
 belongs_to :bird
 belongs_to :user
+
+
+  def display_time
+    if !self.time_date.empty?
+      time_date = DateTime.parse(self.time_date)
+      self.time_date = time_date.strftime('%A %B %e, %Y | %l:%m%p')
+    end  
+  end
 
 end
