099d83d57baf4870141cbc60e9b752362af73cff
chrisjamesr <chrisjames.r@gmail.com>
2017-11-15 22:50:35 -0500

    comment out display_time helper method

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,17 +1,17 @@
 class Sighting < ActiveRecord::Base
 validates_presence_of :user_id, :bird_id, :location, :time_date, allow_blank: false
-after_initialize :display_time
+# after_initialize :display_time
 
 belongs_to :bird
 belongs_to :user
 
 
-  def display_time
-    if !self.time_date.empty?
-      time_date = DateTime.parse(self.time_date)
-      self.time_date = time_date.strftime('%m-%d-%Y')
-    end  
-  end
+  # def display_time
+  #   if !self.time_date.empty?
+  #     time_date = Date.parse(self.time_date)
+  #     self.time_date = time_date.strftime('%m-%d-%Y')
+  #   end  
+  # end
 
 end
 
